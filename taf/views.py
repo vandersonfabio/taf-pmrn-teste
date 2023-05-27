133381f93bc91cc03c521f91d91b4f7f59276238
Vanderson Fábio <32176572+vandersonfabio@users.noreply.github.com>
2023-05-27 12:36:56 -0300

    Correção bug zero nas barras

--- taf/views.py
+++ taf/views.py
@@ -131,7 +131,7 @@ def NotaShuttle(idade, tempo):
 
 def NotaFlexaoBarra(idade, repeticoes):
     flexao_barra = [
-        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0.5,
         1, 2, 3, 4, 5, 6, 7,
         8, 9, 10, 11, 12, 13, 15
     ]
