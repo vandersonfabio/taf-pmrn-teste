8462b22feefe13c0d8553278c3c5577ca856f6ae
vandersonfabio <vanderson.fabio@gmail.com>
2024-02-23 10:25:28 -0300

    Correção de erro no pythonanywhere

--- taf/urls.py
+++ taf/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from taf import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     #path('', views.eacf1_masculino, name='eacf1_masculino'),
+    path('', views.home, name='home'),
     path('eacf1_masculino/', views.eacf1_masculino, name='eacf1_masculino'),
     path('eacf6_masculino/', views.eacf6_masculino, name='eacf6_masculino'),
     path('resultado/', views.resultado, name='resultado'),
